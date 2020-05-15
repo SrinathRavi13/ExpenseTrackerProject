6752cf55a047f615c4e8e5718b7ea9f5d584eeed
SrinathRavi13 <39830189+SrinathRavi13@users.noreply.github.com>
2020-05-15 19:07:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-//import Home from './components/Home'
-//import Home from './react_components/Home'
-
 import HomePage from './components/HomePage'
 
 const App = () => (
